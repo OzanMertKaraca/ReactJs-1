40f93c654bbe276f91173a58c097d0b8c4246944
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-06-03 13:31:08 +0300

    jsx formatı kullanım örnekleri

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-     Hello World
-    </div>
-  );
+class App extends Component  {
+    render()
+    {
+        const sayi=10;
+        const dogru=false;
+        return (
+            <div className="App">
+                Hello World
+                <h1>Ozi</h1>
+                <label htmlFor=""></label>
+                <h3>{4*2}</h3>
+                {"react başlangıç".toUpperCase()}
+                <h4>{sayi}</h4>
+                {
+                    dogru ? <p>Evet</p> : <p>Hayır</p>
+                }
+            </div>
+        );
+    }
 }
-
 export default App;
