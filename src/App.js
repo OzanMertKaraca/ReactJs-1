6d047a8e990143dee8932234f08b6deeb7335793
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-06-03 13:47:44 +0300

    ilk componentimi oluşturdum

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React,{ Component } from 'react';
+import Cmp from   "./ilkcmpnt";
 import './App.css';
 
 
 class App extends Component  {
     render()
     {
-        const sayi=10;
-        const dogru=false;
+
         return (
             <div className="App">
                 Hello World
-                <h1>Ozi</h1>
-                <label htmlFor=""></label>
-                <h3>{4*2}</h3>
-                {"react başlangıç".toUpperCase()}
-                <h4>{sayi}</h4>
-                {
-                    dogru ? <p>Evet</p> : <p>Hayır</p>
-                }
+              <Cmp />
+
             </div>
+
         );
     }
 }
---
+++ src/ilkcmpnt.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Cmp extends Component {
+    render()
+    {
+        return(
+        <div>
+            <h1>React Öğreniyorum</h1>
+            <form action="">
+                <input type="text"/>
+                <button>Kaydet</button>
+            </form>
+        </div>
+        );
+    }
+}
+export default Cmp;
