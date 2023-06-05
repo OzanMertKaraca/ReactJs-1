ddff7f7e3d75302c8defbcf6ed4550199906aa51
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-06-05 21:26:08 +0300

    dizi elemanlarını propsa göndermek

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ import './newcss.css';
 class App extends Component  {
     render()
     {
-        const tittle="React Courses";
-        const description="Yeni Başlangıçlar";
+        const dizim=[
+            {
+                title:"Mutluyum",
+                description:"Umutluyum"
+            },
+            {
+                title: "Yeniden",
+                description: "Başlıyoruz"
+            },
+            {
+                title: "Adım Adım",
+                description: "Sürekli İleri"
+            }
+        ]
+
         return (
             <div className="App">
 
-                <Cmp baslik={tittle} aciklama={description} />
+                <Cmp baslik={dizim[0].title} aciklama={dizim[0].description} />
+                <Cmp baslik={dizim[1].title} aciklama={dizim[1].description} />
+                <Cmp baslik={dizim[2].title} aciklama={dizim[2].description} />
 
             </div>
 
--- src/ilkcmpnt.js
+++ src/ilkcmpnt.js
@@ -3,20 +3,11 @@ import React,{Component} from 'react';
 class Cmp extends Component {
     render()
     {
-        //Props Name isimlendirilme ve yazdırılmada aynı olması gerekir.
-        console.log(this.props)
-        const{baslik,aciklama}=this.props;
+
         return(
         <div>
             <h1>{this.props.baslik}</h1>
             <p>{this.props.aciklama}</p>
-            <h2>React Öğreniyorum</h2>
-            <form action="">
-                <input type="text"/>
-                <button>Kaydet</button>
-            </form>
-            <h1>{baslik}</h1>
-            <p>{aciklama}</p>
         </div>
         );
     }
