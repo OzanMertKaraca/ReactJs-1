0a9df10dd7d7c3bb7d9057c43d20d1dc2dd44f39
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-06-20 22:06:27 +0300

    Comment Project-1.2 : faker js kullanarak fotoğrafları çekmeye çalıştım fakat hatayı dahaca bulamadım çözene kadar profil resimlerini göstermeyeceğim.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
+//import faker from "faker";
 
 const App = () => {
 
     return(
         <div className="ui comments">
             <div className="comment">
-                <a className="avatar">
-                    <img src="/images/avatar/small/stevie.jpg"/>
-                </a>
+                {/*  <a className="avatar" href="/">  */}
+                    <img alt="" src="" /> {/*src={faker.image.avatar()} resimleri random göstermek için yazdık fakat
+                     hata aldığımdan dolayı resimleri göstermeyecğim hatayı bakıyorum daha tam çözemedim çözünce döneceğim*/}
+                {/*  </a> */}
                 <div className="content">
-                    <a className="author">Stevie Feliciano</a>
+                    <a className="author" href="/">Ozan Mert Karaca</a>
                     <div className="metadata">
                         <div className="date">2 days ago</div>
-                        <div className="rating">
+                         <div className="rating">
                             <i className="star icon"></i>
-                            5 Faves
+                            5 Star
                         </div>
                     </div>
                     <div className="text">
-                        Hey guys, I hope this example comment is helping you read this documentation.
+                        Daha Başındayız...
                     </div>
                 </div>
             </div>
