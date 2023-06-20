0198350d429f15062b0646ebe19c30f814fd1065
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-06-20 21:20:00 +0300

    Comment Project-1.1 : Semantic UI  projeye dahil edilmiştir.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import React from 'react';
 const App = () => {
 
     return(
-        <div>
-             Meraba !!!
+        <div className="ui comments">
+            <div className="comment">
+                <a className="avatar">
+                    <img src="/images/avatar/small/stevie.jpg"/>
+                </a>
+                <div className="content">
+                    <a className="author">Stevie Feliciano</a>
+                    <div className="metadata">
+                        <div className="date">2 days ago</div>
+                        <div className="rating">
+                            <i className="star icon"></i>
+                            5 Faves
+                        </div>
+                    </div>
+                    <div className="text">
+                        Hey guys, I hope this example comment is helping you read this documentation.
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
