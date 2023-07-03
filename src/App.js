fccfca3920f16c91f779fb9147b8bd74497b3296
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-03 21:24:47 +0300

    Componentde parent child kullanımı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Comment from "./Comment";
+import Card from "./card";
+import "./App.css";
 //import faker from "faker";
 
 const App = () => {
 
     return(
         <div className="ui comments">
-          <Comment name="Ozan Mert" time="3" star="5" comment="fight to the end"/>
-            <Comment name="Semiramis" time="4" star="5"  comment="I never give up"/>
+            <Card>
+                <Comment name="Ozan Mert" time="3" star="5" comment="fight to the end"/>
+            </Card>
+            <Card>
+                <Comment name="Semiramis" time="4" star="5"  comment="I never give up"/>
+
+            </Card>
+
+
+
         </div>
     )
 }
